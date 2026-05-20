0be6520d2d2aa4dd4da11b2a3763f9306a10814b
aaronkarr <aaron@aaronkarr.com>
2026-05-19 21:52:24 -0400

    extract json custom field values

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import pandas as pd
 
+pd.set_option({"display.max_rows": 100, "display.max_columns": 50})
+
+filepath = "data/gift_designations_fy26.csv"
+
 
 def load_df():
-    df = pd.read_csv("data/gift_designations_fy26.csv")
+    df = pd.read_csv(filepath)
+    # df["REVENUE_STREAM"] = df.PROJECT_CUSTOM_FIELD_VALUES_JSON.str.replace(
+    #     r'[\{\}"\n]', "", regex=True
+    # )
+    # df.REVENUE_STREAM = df.REVENUE_STREAM.str.replace(
+    #     r".*Revenue Stream: (\w+)", r"\1", regex=True
+    # )
+
     return df
+
+
+def add_custom_field(custom_values_col, key):
+    pattern = rf".*{key}: (\w+)"
+    print(pattern)
+    # repl = r"\1"
+    trimmed = custom_values_col.str.replace(r'[\{\}"\n]', "", regex=True)
+    series = trimmed.str.replace(pattern, r"\1", regex=True)
+    return series
 
 
 def main():
     df = load_df()
-    print(df.info())
+    df["REVENUE_STREAM"] = add_custom_field(
+        df.PROJECT_CUSTOM_FIELD_VALUES_JSON, "Revenue Stream"
+    )
+    print(df.REVENUE_STREAM.value_counts())
+    # print(df.PROJECT_CUSTOM_FIELD_VALUES_JSON.value_counts())
 
 
 if __name__ == "__main__":
