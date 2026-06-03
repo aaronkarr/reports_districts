e1254acf971a2074b743f8869ea135b8fa6b847c
aaronkarr <aaron@aaronkarr.com>
2026-06-03 13:49:51 -0400

    not so many fields

--- main.py
+++ main.py
@@ -3,69 +3,75 @@ import numpy as np
 
 pd.set_option({"display.max_rows": 100, "display.max_columns": 50})
 
-filepath = "data/gift_designations_fy26.csv"
-cols = [
-    "ID",
-    "GIFT_ID",
-    # "FT_GIFT_ID",
+gifts_filepath = "data/gift_designations_2025.csv"
+churches_filepath = "data/alliance_churches.csv"
+projects_filepath = "data/project_reporting_categories.csv"
+
+usecols = [
     "GIFT_DATE",
-    "GIFT_CUSTOM_FIELD_VALUES_JSON",
     "GIFT_DESIGNATION_ID",
     "CONVERTED_AMOUNT_DESIGNATED",
-    # "DM_PROJECT_ID",
     "PROJECT_ID",
-    "PROJECT_NAME",
-    "PROJECT_TYPE",
-    # "LOCATION",
-    "PROJECT_CUSTOM_FIELD_VALUES_JSON",
     "CONTACT_ID",
-    # "DM_CONTACT_ID",
-    "CONTACT_NAME",
-    "CONTACT_TYPE",
+    "GIFT_CUSTOM_FIELD_VALUES_JSON",
     "CONTACT_TAG_LIST",
     "CONTACT_CUSTOM_FIELD_VALUES_JSON",
-    "PASSTHROUGH_CONTACT_ID",
-    # "DM_PASSTHROUGH_CONTACT_ID",
-    "PASSTHROUGH_CONTACT_NAME",
-    "PASSTHROUGH_CONTACT_TYPE",
-    "PASSTHROUGH_CONTACT_TAG_LIST",
-    "PASSTHROUGH_CONTACT_CUSTOM_FIELD_VALUES_JSON",
-    "GIFT_CREATED_AT_UTC",
 ]
 
 drop_cols = [
     "GIFT_CUSTOM_FIELD_VALUES_JSON",
-    "PROJECT_CUSTOM_FIELD_VALUES_JSON",
     "CONTACT_TAG_LIST",
     "CONTACT_CUSTOM_FIELD_VALUES_JSON",
-    "PASSTHROUGH_CONTACT_TAG_LIST",
-    "PASSTHROUGH_CONTACT_CUSTOM_FIELD_VALUES_JSON",
+    "CONVERTED_AMOUNT_DESIGNATED",
+    "CHURCH_CODE",
+    "CONTACT_ID",
+    "SOFT_CREDIT_SELECTION",
+    "SOFT_CREDIT_CHURCH",
+    "SOFT_CREDIT_CHURCH_CODE",
+    "ALLIANCE_CHURCH",
 ]
 
+dtypes = {
+    "GIFT_DESIGNATION_ID": "str",
+    "PROJECT_ID": "str",
+    "CONTACT_ID": "str",
+}
 
-def load_df():
-    df = pd.read_csv(filepath, usecols=cols)
-    df["REVENUE_STREAM"] = add_custom_field(
-        df.PROJECT_CUSTOM_FIELD_VALUES_JSON, "Revenue Stream"
+
+def load_gift_df():
+    df = pd.read_csv(
+        gifts_filepath,
+        parse_dates=["GIFT_DATE"],
+        usecols=usecols,
+        dtype=dtypes,
     )
-    df["SOFT_CREDIT_SELECTION"] = add_custom_field(
+    df["AMOUNT"] = df.CONVERTED_AMOUNT_DESIGNATED
+    # df["REVENUE_STREAM"] = add_custom_field(
+    #     df.PROJECT_CUSTOM_FIELD_VALUES_JSON, "Revenue Stream"
+    # )
+    df["SOFT_CREDIT_SELECTION"] = add_custom_col(
         df.GIFT_CUSTOM_FIELD_VALUES_JSON, "Soft Credit"
-    )
-    df["SOFT_CREDIT_CHURCH"] = add_custom_field(
+    ).eq("Yes")
+    df["SOFT_CREDIT_CHURCH"] = add_custom_col(
         df.GIFT_CUSTOM_FIELD_VALUES_JSON, "Soft Credit Church"
     )
-    df["CHURCH_CODE"] = add_custom_field(
+    df["SOFT_CREDIT_CHURCH_CODE"] = df.SOFT_CREDIT_CHURCH.str.extract(r".*\[(.*)\]")
+    df["CHURCH_CODE"] = add_custom_col(
         df.CONTACT_CUSTOM_FIELD_VALUES_JSON, "Church Code"
     )
-    df["CHURCH_DISTRICT"] = tag_to_col(df.CONTACT_TAG_LIST, "District")
+    # df["CHURCH_DISTRICT"] = tag_to_col(df.CONTACT_TAG_LIST, "District")
     df["ALLIANCE_CHURCH"] = df.CONTACT_TAG_LIST.str.contains(
         "alliance church", case=False
     )
+    df["YEAR"] = df.GIFT_DATE.dt.year
+    df["MONTH"] = df.GIFT_DATE.dt.month
+    df["CREDIT_TO"] = add_credit_col(df)
+
     df.drop(columns=drop_cols, inplace=True)
     return df
 
 
-def add_custom_field(custom_values_col, key):
+def add_custom_col(custom_values_col, key):
     pattern = rf'.*"{key}":\s*"([^"]+)".*'
     # Add surrounding quotes to null in raw column to align with non-null string format
     null_quotes = custom_values_col.str.replace("null", '"null"')
@@ -86,30 +92,104 @@ def tag_to_col(custom_tags_col, key):
     return series
 
 
-def main():
-    df = load_df()
+def add_credit_col(gift_df):
+    def get_credit_value(df):
+        if df.ALLIANCE_CHURCH:
+            return df.CHURCH_CODE
+        if df.SOFT_CREDIT_SELECTION:
+            return df.SOFT_CREDIT_CHURCH_CODE
+        return "no one"
 
-    # print(
-    #     df[df.SOFT_CREDIT_SELECTION != "Yes"][
-    #         ["CONTACT_NAME", "SOFT_CREDIT_CHURCH"]
-    #     ].value_counts()
-    # )
-    by_district = df.groupby(["CHURCH_DISTRICT", "CHURCH_CODE"])[
-        ["CONVERTED_AMOUNT_DESIGNATED"]
-    ].sum()
-    # by_district.to_csv("out/by_district.csv")
-    by_rev_stream = df.groupby(["REVENUE_STREAM"], dropna=False)[
-        "CONVERTED_AMOUNT_DESIGNATED"
-    ].sum()
-    print(by_rev_stream)
-    by_streamless_project = (
-        df[df.REVENUE_STREAM.isna()]
-        .groupby("PROJECT_NAME")["CONVERTED_AMOUNT_DESIGNATED"]
-        .sum()
+    series = gift_df.apply(get_credit_value, axis=1)
+    return series
+
+
+def merge_and_group_by_month(gifts_df, churches_df, projects_df):
+    merged = pd.merge(gifts_df, projects_df, how="left", on="PROJECT_ID")
+    gifts_by_month = (
+        merged[
+            [
+                "YEAR",
+                "MONTH",
+                "CREDIT_TO",
+                # "CHURCH_NAME",
+                # "CHURCH_CODE",
+                # "CHURCH_STATUS",
+                # "DISTRICT_NAME",
+                # "ASSOCIATION_NAME",
+                "AMOUNT",
+                "CATEGORY",
+            ]
+        ]
+        .groupby(
+            [
+                "YEAR",
+                "MONTH",
+                "CREDIT_TO",
+                # "CHURCH_NAME",
+                # "CHURCH_CODE",
+                # "CHURCH_STATUS",
+                # "DISTRICT_NAME",
+                # "ASSOCIATION_NAME",
+                "AMOUNT",
+                "CATEGORY",
+            ],
+            as_index=False,
+            dropna=False,
+        )
+        .agg({"AMOUNT": "sum"})
     )
-    print(by_streamless_project)
-    print("----------")
-    print(df.columns)
+    gifts_by_month = pd.merge(
+        gifts_by_month,
+        churches_df,
+        how="outer",
+        left_on="CREDIT_TO",
+        right_on="CHURCH_CODE",
+    )
+    # gifts_by_month = gifts_by_month.astype({"MONTH": "int", "YEAR": "int"})
+    # print(gifts_by_category[gifts_by_category.CATEGORY.isna()])
+    # gifts_by_month.CREDIT_TO.rename("CHURCH_CODE")
+    return gifts_by_month
+
+
+# def generate_reports(gifts_by_month_df):
+#         for month in gifts_by_month_df.MONTH:
+#             for district in gifts_by_month_df.DISTRICT_NAME:
+
+
+def main():
+    gifts = load_gift_df()
+
+    churches = pd.read_csv(churches_filepath)
+    projects = pd.read_csv(projects_filepath, dtype={"PROJECT_ID": "string"})
+
+    gifts_by_month = merge_and_group_by_month(gifts, churches, projects)
+
+    # gifts_by_month = pd.merge(
+    #     churches,
+    #     gifts_by_month,
+    #     how="outer",
+    #     left_on="CHURCH_CODE",
+    #     right_on="CREDIT_TO",
+    # )
+    # gifts_by_month = gifts_by_month.merge(projects, how="left", on=["PROJECT_ID"])
+
+    # gifts_by_month.to_csv("out/gifts_by_month_with_cat.csv")
+    print(gifts.info())
+    print("--------")
+    print(gifts_by_month.info())
+    print("--------")
+    print("--------")
+    print(f"gifts total:          {gifts.AMOUNT.sum()}")
+    # print(f"merged total:         {merged.AMOUNT.sum()}")
+    print(f"gifts_by_month total: {gifts_by_month.AMOUNT.sum()}")
+    gifts_by_month.to_csv("out/gifts_by_month.csv")
+    print("--------")
+    print(
+        gifts_by_month[
+            gifts_by_month.CATEGORY.isna() & gifts_by_month.AMOUNT > 0
+        ].head()
+    )
 
 
 if __name__ == "__main__":
